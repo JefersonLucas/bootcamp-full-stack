81ca39ebbdb409d5305fc72500339a1d21bf6fe3
Jeferson Lucas <kas_sants@hotmail.com>
2021-03-18 18:39:39 -0300

    12 Viewing Module 01 Challenge Users

--- desafio-01/frontend/js/index.js
+++ desafio-01/frontend/js/index.js
@@ -2,14 +2,9 @@
 
 // Objeto de estado global da aplicação
 const globalState = {
-	users: [],
 	filteredUsers: [],
-	loading:  function(value) {
-		let loading = loader;
-		loading = value ? 
-			loading.classList.add("active-progress") :
-			loading.classList.remove("active-progress");
-	}
+	statisticsUsers: [],
+	filter: "",
 }
 
 // Variáveis de estado global da aplicação
@@ -18,171 +13,152 @@ const loader = document.querySelector("#loader");
 const usersForm = document.querySelector("#users-form");
 const usersSearch = document.querySelector("#users-search");
 const submitButton = document.querySelector("#submit-button");
-const filteredUsers = document.querySelector("#filtered-users");
-const statisticsUsers = document.querySelector("#statistics-users");
+const globalFilteredUsers = document.querySelector("#filtered-users");
+const globalStatisticsUsers = document.querySelector("#statistics-users");
 
 // Tudo começa por aqui, a invocação desta função é feita na última linha de código deste arquivo
-function start() {
-	// Esse método efetua o foco no input de busca de usuário
-	usersSearch.focus();
+async function start() {
 	// Monitora se o input tem algum valor
-	usersSearch.addEventListener("input", event => {
-		let valueInput = event.target.value;
-		const { loading } = globalState;
-
-		valueInput = valueInput ?	loading(true) : loading(false)
-
+	usersSearch.addEventListener("keyup", function(event){
+		globalState.filter = event.target.value;
+		returnAllUsers();
 	});
 	// Monitora se existe algum evento submit no formulário
-	usersForm.addEventListener("submit", handleSubmit);
-}
-
-// Função que atua sobre o clique no submit
-function handleSubmit(event) {
-
-	const filter = event.target[0].value;
-	const { loading } = globalState;
-
-	// Previne o evento padrão do formulário
-	event.preventDefault();
- 
-	// Verifica se há algum valor no input.
-	if(event.target[0].value) {
-		// Se houver, inclui uma nova classe que ativa a barra de progresso.
-		loading(true);
-		// Chamada da função que faz a requisição da API
-		fetchUsers(api);
-		// Chamada das funções que montam a lista de usuários e estatísticas 
-		filteredUsers.appendChild(returnFilteredUsers(filter));
-		statisticsUsers.appendChild(returnStatisticsUsers(filter));
-	}
-	else {
-		// Se não, remove a barra de progresso.
-		loading(false)
-	}
+	usersForm.addEventListener("submit", function(event) {
+		event.preventDefault();
+		globalState.filter = event.target[0].value;
+		returnAllUsers();
+	});
+	// Chamando a função que faz o fetch da API que contém os usuários
+	await fetchUsers();
+	// Retornando os usuários
+	returnAllUsers()
 }
 
 // Fazendo um async/await na API levantada em http://localhost:3001/users
-async function fetchUsers(url) {
-
-	const response = await fetch(url);
+async function fetchUsers() {
+	// Obtendo a API em formato JSON
+	const response = await fetch(api);
 	const json = await response.json()
-
 	// Fazendo um mapeamento em da API 
-	const fetchUsers = json.map((user) => {
-		
-		// Obterndo: name, picture, dob e gender da API
-		const { name, picture, dob, gender } = user;
-		// Retornando os valores
+	const fetchUsers = json.map(user => {
+	// Obterndo: name, picture, dob e gender da API
+	const { name, picture, dob, gender } = user;
+	// Retornando os valores
 		return { name, picture, dob, gender }
 	});
-
 	// Inserindo valores mapeados no Objeto de estado global
-	globalState.users = [...fetchUsers];
+	globalState.filteredUsers = [...fetchUsers];
+	globalState.statisticsUsers = [...fetchUsers];
+}
 
+// Função que retorna todos os usuários
+function returnAllUsers() {
+	returnFilteredUsers();
+	returnStatisticsUsers();
+}
+
+// Função auxiliar de `returnFilteredUsers()` que filtra os usuários
+function renderUserFilter(user) {
+	const { picture, name, dob } = user;
+
+	return `
+		<li class="collection-item avatar">
+			<img src="${picture.thumbnail}" alt="" class="circle">
+			<span class="title">
+				${name.first} ${name.last}
+			</span>
+			<p> 
+				${dob.age} anos
+			</p>
+			<span class="secondary-content"><i class="material-icons">person</i></span>
+		</li>
+	`
 }
 
 // Retorna as listas de Usuários Filtrados
-function returnFilteredUsers(filter) {
+function returnFilteredUsers() {
 	// Extraindo todos os usuários do objeto global
-	const { users } = globalState;
-	// Criação dos elementos
-	const ul = document.createElement("ul");
-	const li = document.createElement("li")
-	const h4 = document.createElement("h4");
-	// Atribuição de classes e elementos internos
-	ul.className = "collection with-header";
-	li.className = "collection-header";
-	h4.innerText = "Usuário(s) filtrado(s)";
-	// Inserção dos elementos filhos
-	li.appendChild(h4);
-	ul.appendChild(li);
+	const { filteredUsers, filter } = globalState;
+	// Fazendo o filtro dos usuários pelo nome
+	const userFiltered = filteredUsers.filter(user => user.name.first === filter || user.name.last === filter);
+	// Fazendo um mapeamento dos usuários filtrados
+	const nameFilter = userFiltered
+		.map((user) => {
+			return renderUserFilter(user);
+	})
+	// Renderização dos usuários
+	const filtering = (
+		`
+			<ul class="collection with-header">
+				<li class="collection-header">
+					<h4 class="center">${nameFilter.length} Usuário(s) filtrado(s)</h4>
+				</li>
+				${nameFilter}
+		</ul> 
+	`);
+	// Retorno dos usuários filtrados
+	return globalFilteredUsers.innerHTML = filtering;
+}
 
-	console.log(filter)
+// Função auxiliar de `returnStatisticsUsers()` que faz as estastísticas os usuários
+function renderUserStatistics(female, male, ageSum, averageAges) {
 
-	// Retorno do elemento pai com os elementos filhos dentro do elemento pai
-	return ul;
-
-	/*
-	Usuários filtrados
-	
-	<ul class="collection with-header">
-		<li class="collection-header">
-			<h4>Usuário(s) filtrado(s)</h4>
+	return `
+		<li class="collection-item">
+			<div>Sexo masculino: ${male.length}
+				<span class="secondary-content"><i class="material-icons">wc</i></span>
+			</div>
 		</li>
-
-		<li class="collection-item avatar">
-			<img src="https://randomuser.me/api/portraits/thumb/women/78.jpg" alt="" class="circle">
-			<span class="title">
-				Nome 01
-			</span>
-			<p>
-				01 anos
-			</p>
-			<p>
-				Cidade 01
-			</p>
-			<a href="#!" class="secondary-content"><i class="material-icons">person</i></a>
+		<li class="collection-item">
+			<div>Sexo feminino: ${female.length}
+				<span class="secondary-content"><i class="material-icons">wc</i></span>
+			</div>
 		</li>
-		<li class="collection-item avatar">
-			<img src="https://randomuser.me/api/portraits/thumb/women/79.jpg" alt="" class="circle">
-			<span class="title">
-				Nome 02
-			</span>
-			<p>02 anos</p>
-			<p>Cidade 02</p>
-			<a href="#!" class="secondary-content"><i class="material-icons">person</i></a>
+		<li class="collection-item">
+			<div>Soma das idades: ${ageSum}
+				<span class="secondary-content"><i class="material-icons">plus_one</i></span>
+			</div>
 		</li>
-	</ul> 
-*/
+		<li class="collection-item">
+			<div>Média das idades: ${averageAges}
+				<span class="secondary-content"><i class="material-icons">trending_up</i></span>
+			</div>
+		</li>
+	`
 }
 
 // Retorna as lista de Estatísticas do Usuário
-function returnStatisticsUsers(filter) {
+function returnStatisticsUsers() {	
 	// Extraindo todos os usuários do objeto global
-	const { users } = globalState;
-	// Criação dos elementos
-	const ul = document.createElement("ul");
-	const li = document.createElement("li")
-	const h4 = document.createElement("h4");
-	// Atribuição de classes e elementos internos
-	ul.className = "collection with-header";
-	li.className = "collection-header";
-	h4.innerText = "Estatística(s)";
-	// Inserção dos elementos filhos
-	li.appendChild(h4);
-	ul.appendChild(li);
-	
-	console.log(filter)
+	const { statisticsUsers, filter } = globalState;
+	// Fazendo o filtro dos usuários pelo nome
+	const userFiltered = statisticsUsers.filter(user => user.name.first === filter || user.name.last === filter);
+	// Variáveis auxiliares para as estastísticas
+	const female = userFiltered.filter(user => user.gender === "female");
+	const male = userFiltered.filter(user => user.gender === "male");
+	const ageSum = userFiltered.reduce((accumulator, current) => {
+		return accumulator + current.dob.age;
+	}, 0);
+	const averageAges = Math.floor(ageSum / userFiltered.length);
+	let render = ""
+	// Verifica se existe algum filtro
+	if(userFiltered.length > 0) {
+		render = renderUserStatistics(female, male, ageSum, averageAges);
+	}
 
-
-	// Retorno do elemento pai com os elementos filhos dentro do elemento pai
-	return ul;
-
-	/*
-	Estatísticas
-
-	<ul class="collection with-header">
-
-	<li class="collection-header ">
-		<h4>Estatística(s)</h4>
-	</li>
-
-	<li class="collection-item">
-		<div>Sexo masculino: 0<a href="#!" class="secondary-content"><i class="material-icons">wc</i></a></div>
-	</li>
-	<li class="collection-item">
-		<div>Sexo feminino: 0<a href="#!" class="secondary-content"><i class="material-icons">wc</i></a></div>
-	</li>
-	<li class="collection-item">
-		<div>Soma das idades: 0<a href="#!" class="secondary-content"><i class="material-icons">plus_one</i></a></div>
-	</li>
-	<li class="collection-item">
-		<div>Média das idades: 0<a href="#!" class="secondary-content"><i class="material-icons">trending_up</i></a></div>
-	</li>
-
-	</ul> 
-*/
+	// Renderização dos usuários
+	const statistics = (
+		`
+		<ul class="collection with-header">
+			<li class="collection-header ">
+				<h4 class="center">Estatística(s)</h4>
+			</li>
+			${render}
+		</ul> 
+	`);
+	// Retorno das estastísticas dos usuários
+	return globalStatisticsUsers.innerHTML = statistics;
 }
 
 // Iniciando o app
