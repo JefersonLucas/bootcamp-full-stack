fd00074d9f104cd0de481e39059ba5f7eb2475f5
Jeferson Lucas <kas_sants@hotmail.com>
2021-04-22 12:36:12 -0300

    53 Module 02 activity 02 completed

---
+++ trabalho-pratico-04/models/accountModel.js
@@ -0,0 +1,36 @@
+// Importação de bibliotecas
+import mongoose from "mongoose";
+
+// Criação do modelo
+const accountSchema = mongoose.Schema({
+  agencia: {
+    type: Number,
+    require: true,
+  },
+  conta: {
+    type: Number,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  balance: {
+    type: Number,
+    require: true,
+  },
+  // Função de validação
+  validate(balance) {
+    if (balance < 0) {
+      throw new Error("Valor negativo para o saldo não é permitido!");
+    }
+  },
+  // Definindo o valor mínimo
+  min: 0,
+});
+
+// Definindo o modelo da coleção
+const accountModel = mongoose.model("account", accountSchema, "account");
+
+// Exportando o modelo
+export { accountModel };
