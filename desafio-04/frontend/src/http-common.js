8689efe9b69809e8e8f58b794e2c649a84e239c0
Jeferson Lucas <kas_sants@hotmail.com>
2021-04-28 08:58:05 -0300

    63 Module 04 challenge frontend implemented

--- desafio-04/frontend/src/http-common.js
+++ desafio-04/frontend/src/http-common.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: "https://desafio-04-backend.herokuapp.com/", // Link de origem do backend
   headers: {
-    'Content-type': 'application/json',
+    "Content-type": "application/json",
   },
 });
